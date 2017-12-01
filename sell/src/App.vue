52e71df82f44638f140216facf5c055d586295b7
lovelytong <lovelytong@lovelytongdeMacBook-Air.local>
2017-12-01 09:39:58 +0800

    组建拆分
    在static中增加reset.css;
    将页面分为header，tab和content三部分；
    因为编译不能通过，将header中style设置的部分暂时删掉了

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <!--<img src="./assets/logo.png">-->
-    <router-view/>
+    <v-header></v-header>
+    <div class="tab">
+      I am tab!
+    </div>
+    <div class="content">
+      I am content!
+    </div>
+    
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app'
-}
+<script >
+import header from './components/header/header.vue';
+export default{components:
+  {'v-header': header}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style >
+
 </style>
--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -1,1 +1,10 @@
-header
+<template>
+  <div class="header">
+  	 我是header！</div>
+</template>
+<script type="text/ecmascript-6">
+export default{};
+</script>
+<style >
+</style>
+  
