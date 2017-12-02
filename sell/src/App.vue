ed949a10fbe9114b7197f4546f35cacc340187ec
lovelytong <lovelytong@lovelytongdeMacBook-Air.local>
2017-12-02 16:34:18 +0800

    Vue-router done
    用router实现商品／评价／商家三个页面的切换效果
    vue-router1.0至2.0语法变化较大

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <div id="app">
+  <div>
     <v-header></v-header>
-    <div class="tab">
-     <div class="tab-item">商品</div>
-     <div class="tab-item">评论</div>
-     <div class="tab-item">商家</div>
+  <div class="tab">
+    <div class="tab-item">
+      <router-link to="/goods">商品</router-link>
     </div>
-    <div class="content">
-      I am content!
+    <div class="tab-item">
+      <router-link to="/ratings">评论</router-link>
     </div>
-    
+    <div class="tab-item">
+      <router-link to="/seller">商家</router-link>
+     </div>
+    </div>
+    <router-view></router-view>   
   </div>
 </template>
 
@@ -21,14 +24,13 @@ export default{components:
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  #app
-    .tab
-      display: flex
-      width: 100%
-      height: 40px
-      line-height: 40px
-      .tab-item
-        flex: 1
-        text-align: center
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    .tab-item
+      flex: 1
+      text-align: center
 
 </style>
