6d825aedf70d475b04e06b597a4a37a33035df2e
lovelytong <lovelytong@lovelytongdeMacBook-Air.local>
2017-12-07 21:44:07 +0800

    header components done

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -14,10 +14,25 @@
   	 			<span class="icon" :class="classMap[seller.supports[0].type]"></span>
   	 			<span class="text">{{seller.supports[0].description}}</span>
   	 		</div>
-  	 	</div>	 	
+  	 		<div v-if="seller.supports" class="support-count">
+	  	 		<span class="count">{{seller.supports.length}}个</span>
+	  	 		<i class="icon-keyboard_arrow_right"></i>
+  	 		</div>	
+  	 	</div>
+  	 	<div v-if="seller.supports" class="support-count">
+  	 		<span class="count">{{seller.supports.length}}个</span>
+  	 		<i class="icon-keyboard_arrow_right"></i>
+  	 	</div>	
+  	 	 	
   	 </div>
 
-  	 <div class="bulletin-wrapper"></div>
+  	 <div class="bulletin-wrapper">
+  	 	<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+  	 	<i class="icon-keyboard_arrow_right"></i>>
+  	 </div>
+  	 <div class="background">
+  	 	<img :src="seller.avatar" width="100%" height="100%">
+  	 </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -34,10 +49,14 @@ export default{
 <style lang="stylus" rel="stylesheet/stylus" >
 	@import "../../common/stylus/mixin"
 	.header
+		position:relative
+		overflow:hidden
 		color:#fff
-		background:#aaa
+		background:rgba(7,17,27,0.5)
 		.content-wrapper
+			position:relative
 			padding:24px 12px 18px 24px
+			font-size:0
 			.avatar
 				display:inline-block
 				vertical-align:top
@@ -89,9 +108,62 @@ export default{
 							bg-image('special_1')
 					.text
 						line-height:12px
-						font-size:12px
+						font-size:10px
 						vertical-align:top
+			.support-count
+				position:absolute
+				right:12px
+				bottom:14px
+				padding:0 8px
+				height:24px
+				line-height:24px
+				border-radius:14px
+				background:rgba(0,0,0,0.2)
+				text-align:center				
+				.count
+					vertical-align:top
+					font-size:10px
+				.icon-keyboard_arrow_right
+					margin-left:2px
+					line-height:24px
+					font-size:10px
+		
+		.bulletin-wrapper
+			position:relative
+			height:28px
+			line-height:28px
+			padding:0 22px 0 12px
+			white-space:nowrap
+			overflow:hidden
+			text-overflow:ellipsis
+			background:rgba(7,17,27,0.2)
+			.bulletin-title
+				display:inline-block
+				vertical-align:top
+				margin-top:8px
+				width:22px
+				height:12px
+				bg-image('bulletin')
+				background-size:22px 12px
+				background-repeat:no-repeat
+			.bulletin-text
+				margin:0 4px
+				font-size:10px
+				vertical-align:top
+			.icon-keyboard_arrow_right
+					position: absolute
+					font-size:10px
+					right:12px
+					top:8px
+		.background
+			position:absolute
+			top:0
+			left:0
+			width:100%
+			height:100%
+			z-index:-1
+			filter: blur(10px)
+			
 
-				
 </style>
   
