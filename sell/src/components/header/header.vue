3123cea99c740cd43035753325655457d155912d
lovelytong <lovelytong@lovelytongdeMacBook-Air.local>
2017-12-08 22:32:06 +0800

    header弹出层和star组件

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -19,32 +19,61 @@
 	  	 		<i class="icon-keyboard_arrow_right"></i>
   	 		</div>	
   	 	</div>
-  	 	<div v-if="seller.supports" class="support-count">
+  	 	<div v-if="seller.supports" class="support-count"  @click="showDetail">
   	 		<span class="count">{{seller.supports.length}}个</span>
   	 		<i class="icon-keyboard_arrow_right"></i>
   	 	</div>	
   	 	 	
   	 </div>
 
-  	 <div class="bulletin-wrapper">
+  	 <div class="bulletin-wrapper" @click="showDetail">
   	 	<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
   	 	<i class="icon-keyboard_arrow_right"></i>>
   	 </div>
   	 <div class="background">
   	 	<img :src="seller.avatar" width="100%" height="100%">
   	 </div>
+  	 <div v-show="detailShow" class="detail">
+  	 	<div class="detail-wrapper clearfix">
+  	 		<div class="detail-main">
+  	 			<h1 class="name">{{seller.name}}</h1>
+                <div class="star-wrapper">
+                	<star :size="48" :score="seller.score"></star>
+                </div>               
+            </div>           
+  	 	</div>
+  	 	<div class="detail-close">
+  	 		<i class="icon-close"></i>
+  	 	</div>
+  	 </div>
+  	 	
+  	 </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
-export default{
-	props: {
-		seller: {
-			type: Object}
+	import star from 'components/star/star';
+	export default{
+		props: {
+			seller: {
+				type: Object}
+			},
+		data() {
+			return {
+				detailShow: false
+			};
 		},
-	created() {
-		this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+		methods: {
+			showDetail() {
+				this.detailShow = true;
+			}
+		},
+		created() {
+			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+		},
+		components: {
+			star
 		}
-};
+	};
 </script>
 <style lang="stylus" rel="stylesheet/stylus" >
 	@import "../../common/stylus/mixin"
@@ -163,6 +192,37 @@ export default{
 			height:100%
 			z-index:-1
 			filter: blur(10px)
+		.detail
+			position: fixed
+			z-index: 100
+			top: 0
+			left: 0
+			width:100%
+			height:100%
+			overflow:auto
+			background:rgba(7,17,27,0.8)
+			.detail-wrapper
+				width:100%
+				min-height: 100%
+				.detail-main
+					margin-top: 64px
+					padding-bottom: 64px
+					.name
+						line-height: 16px
+						text-align: center
+						font-size: 16px
+						font-weight: 700
+					.star-wrapper
+						margin-top: 18px
+						padding: 2px 0
+						text-align: center
+			.detail-close
+				position: relative
+				width: 32px
+				height: 32px
+				margin:-64px auto 0 auto
+				clear: both
+				font-size:32px
 			
 
 </style>
